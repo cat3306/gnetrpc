b9c336077f3eb1ea59cb6174650f865e62c5e098
cat13 <1273014435@qq.com>
2024-03-07 16:57:35 +0800

    update

--- defaultservice.go
+++ defaultservice.go
@@ -24,29 +24,6 @@ func (b *BuiltinService) Alias() string {
 	return BuiltinServiceName
 }
 func (b *BuiltinService) Heartbeat(ctx *protocol.Context, args *string, reply *string, tag struct{}) *CallMode {
-	last, ok := ctx.Conn.GetProperty(lastHeartbeatKey)
-	now := time.Now().UnixMilli()
-	if ok {
-		milli := last.(int64)
-		rpclog.Info(ctx.Conn.Id(), ";", now-milli)
-	}
-	// if ok {
-	// 	milli := last.(int64)
-	// 	rpclog.Info(ctx.Conn.Id(), ";", now-milli)
-	// 	if now-milli < maxHeartbeatFrequency.Milliseconds() {
-
-	// 		msg := fmt.Sprintf("id:%s the heart interval is %dms should be greater than %dms closed it", ctx.Conn.Id(), now-milli, maxHeartbeatFrequency/time.Millisecond)
-	// 		rpclog.Errorf(msg)
-	// 		ctx.Conn.Close("heart interval")
-	// 		os.Exit(0)
-	// 		return nil
-	// 	}
-	// }
-	ctx.Conn.SetProperty(lastHeartbeatKey, now)
-	if b.debug {
-		rpclog.Info(*args)
-	}
-	//rpclog.Info(*args, ctx.Metadata)
 	*reply = *args
 	return CallSelf()
 }
